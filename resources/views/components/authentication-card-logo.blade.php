3d93a91c0c17dbbaf23db77042aa58f1b6f0f297
Smail <b.smailiams@gmail.com>
2024-04-11 00:02:10 +0100

    fix auth logo

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <img alt="Sell3a.Store" loading="lazy" src="http://127.0.0.1:8000/images/logo.png" style="height: 8rem;" class="fi-logo inline-flex mb-4">
+    <img alt="Sell3a.Store" loading="lazy" src="/images/logo.png" style="height: 8rem;" class="fi-logo inline-flex mb-4">
 </a>
