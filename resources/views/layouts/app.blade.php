76eefb1c461d0e7d0b22ed41fce82266b87ddbe6
Smail <b.smailiams@gmail.com>
2024-04-11 00:11:00 +0100

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     <body class="font-sans antialiased">
         <x-banner />
 
-        {{-- <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @livewire('navigation-menu')
 
             <!-- Page Heading -->
@@ -36,7 +36,7 @@
             <main>
                 {{ $slot }}
             </main>
-        </div> --}}
+        </div>
 
         @stack('modals')
 
